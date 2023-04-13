0f489a637e1ee36196c9c0b2d6e52b1d7c0eaa34
jrolls81 <105545633+jrolls81@users.noreply.github.com>
2023-04-13 17:47:44 -0500

    Initial commit

---
+++ src/main/java/com/outreachmaps/outreachmaps/OutreachMapsApplication.java
@@ -0,0 +1,13 @@
+package com.outreachmaps.outreachmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutreachMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutreachMapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/outreachmaps/outreachmaps/OutreachMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outreachmaps.outreachmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutreachMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
